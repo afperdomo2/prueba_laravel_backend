3f5e7943f46ee8db4c948a78b1c90056dbce6460
alvarado0925 <alvarado0925@gmail.com>
2024-12-03 23:06:36 -0500

    Agregar configuración de base de datos y migraciones para hoteles y habitaciones

---
+++ database/migrations/2024_12_04_033022_create_rooms_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('hotel_id')
+                ->constrained('hotels')
+                ->cascadeOnUpdate()
+                ->restrictOnDelete();
+            $table->enum('type', ['Estándar', 'Junior', 'Suite']);
+            $table->enum('accommodation', ['Sencilla', 'Doble', 'Triple', 'Cuádruple']);
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
